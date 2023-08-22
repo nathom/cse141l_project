2702619d795e6736bcc271cce9df0a4d7c4afef7
Nathan Thomas <nathanthomas707@gmail.com>
2023-08-21 19:04:45 -0700

    Update alu

--- src/alu.sv
+++ src/alu.sv
@@ -1,14 +1,15 @@
 // combinational -- no clock
 // sample -- change as desired
 module alu (
-    input        [2:0] alu_cmd,  // ALU instructions
+    input        [1:0] alu_cmd,  // ALU instructions
     input        [7:0] inA,
     inB,  // 8-bit wide data path
     input              sc_i,     // shift_carry in
     output logic [7:0] rslt,
     output logic       sc_o,     // shift_carry out
     pari,  // reduction XOR (output)
-    zero  // NOR (output)
+    zero,  // NOR (output)
+    neq  // inA != inB
 );
 
   always_comb begin
@@ -16,27 +17,14 @@ module alu (
     sc_o = 'b0;
     zero = !rslt;
     pari = ^rslt;
+    neq  = inA != inB;
     case (alu_cmd)
-      3'b000: // add
-      begin
-        rslt = inA + inB;
-        sc_o = rslt[8];
-      end
-      3'b001: // left rotate
-      begin
-        rslt = {inA[6:0], inA[7]};
-        sc_o = rslt[8];
-      end
-      3'b010: // NAND
-      begin
-        rslt = ~(inA & inB);
-        sc_o = sc_i;  // No shift operation for NAND
-      end
-      3'b011: // subtract
-      begin
-        rslt = inA - inB;
-        sc_o = rslt[8];
-      end
+      2'b00:  // add
+      rslt = inA + inB;
+      2'b01:  // right rotate
+      rslt = {inA[0], inA[1:7]};
+      2'b10:  // NAND
+      rslt = ~(inA & inB);
     endcase
   end
 
