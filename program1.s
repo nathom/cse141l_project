63ebf4b9489ab2794eb0425864e6f37f39ffc160
Taylor Salazar <tesalazar@ucsd.edu>
2023-08-29 13:12:47 -0700

    lowercased some instructions in prorgram 1

--- program1.s
+++ program1.s
@@ -122,48 +122,48 @@ mov r2, OUT         // Now r2 holds lsb rrt(4) & 11110000 |((lsb & 00000001) rrt
 
 
 //Red Bold
-Mov r4, 0b01000000
-And r3, r4		//Mask parity bits to grab p4 bit
-Rot OUT, 10	//Rotates by 10 to get into lowest bit first
-Mov r4, OUT 	//r4 now holds p4 rrt(4)
-Mov OUT, 0b11110000
-And OUT, r4
-Mov r4, OUT	//Finishes p4 rrt(4) & 11110000
-Orr r2, r4
-Mov r2, OUT	//r2 now holds the first 3 or statements (r4 free again)
+mov r4, 0b01000000
+and r3, r4		//Mask parity bits to grab p4 bit
+rot OUT, 10	//Rotates by 10 to get into lowest bit first
+mov r4, OUT 	//r4 now holds p4 rrt(4)
+mov OUT, 0b11110000
+and OUT, r4
+mov r4, OUT	//Finishes p4 rrt(4) & 11110000
+orr r2, r4
+mov r2, OUT	//r2 now holds the first 3 or statements (r4 free again)
 
 
 //Handling p2 bit
-Mov r4, 0b00100000
-And r3, r4		//Mask parity bits to grab p2 bit
-Rot OUT, 3
-Mov r4, OUT 	//r4 now holds p2 rrt(6)since it rotates twice
-Mov OUT, 0b11111100
-And OUT, r4
-Mov r4, OUT	//Finishes p2 rrt(6) & 11111100
-Orr r2, r4
-Mov r2, OUT	//r2 now holds the first 4 or statements (r4 free again)
+mov r4, 0b00100000
+and r3, r4		//Mask parity bits to grab p2 bit
+rot OUT, 3
+mov r4, OUT 	//r4 now holds p2 rrt(6)since it rotates twice
+mov OUT, 0b11111100
+and OUT, r4
+mov r4, OUT	//Finishes p2 rrt(6) & 11111100
+orr r2, r4
+mov r2, OUT	//r2 now holds the first 4 or statements (r4 free again)
 
 
 //Handling p1 bit
-Mov r4, 0b00010000
-And r3, r4		//Mask parity bits to grab p1 bit
-Rot OUT, 7
-Mov r4, OUT 	//r4 now holds p2 rrt(6)since it rotates twice
-Mov OUT, 0b11111110
-And OUT, r4
-Mov r4, OUT	//Finishes p1 rrt(7) & 11111110
-Orr r2, r4
-Mov r2, OUT	//r1 now holds the first 4 or statements (r4 free again)
+mov r4, 0b00010000
+and r3, r4		//Mask parity bits to grab p1 bit
+rot OUT, 7
+mov r4, OUT 	//r4 now holds p2 rrt(6)since it rotates twice
+mov OUT, 0b11111110
+and OUT, r4
+mov r4, OUT	//Finishes p1 rrt(7) & 11111110
+orr r2, r4
+mov r2, OUT	//r1 now holds the first 4 or statements (r4 free again)
 
 
 //Handling p0 case
-Mov r4, 0b00001000
-And r4, r3
-Rot OUT, 3
-Mov r4, OUT		//p4 now holds p0
-Orr r2, r4
-Mov r2, OUT		//r2 now holds the entire expression
+mov r4, 0b00001000
+and r4, r3
+rot OUT, 3
+mov r4, OUT		//p4 now holds p0
+orr r2, r4
+mov r2, OUT		//r2 now holds the entire expression
 
 // TODO Must bring result back into mem[]
 // What does this mean? -> OUT, parity Is this translating properly?
