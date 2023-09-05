14f06bd57693a008128f900d6ad8897cb0efad74
Taylor Salazar <tesalazar@ucsd.edu>
2023-09-04 19:18:24 -0700

    Fixed rotate syntax in program1, wants review

--- program1.s
+++ program1.s
@@ -13,25 +13,25 @@ add r0, r2  // OUT = i + 1
 ldr r2, OUT // r2 = mem[OUT] = mem[i+1]
 
 
-// This is not done line 21 is incorrect
 // Move p8 value into r3 (*each bit in r3 will hold a parity bit)
 mov r3, 0b11110000
 and r2, r3         // OUT = lsb & 11110000, parity = ^OUT
-xor r1, OUT        // parity = ^(msb ^ (lsb & 11110000))
-
-// Fix with parity register par
-mov r3, 1
-rot OUT, r3
+xor r1, OUT        // (msb ^ (lsb & 11110000))
+mov r3, par		   //r3 now has p8 in lsb
+mov r4, 1
+rot r3, r4
 mov r3, OUT
 
 
-// Move p4 value into r3
 mov r4, 0b10001110
 and r2, r4 // OUT = lsb & 10001110, parity = ^OUT
 xor r1, OUT // parity = ^(msb ^ (lsb & 10001110))
-
-// Fix with parity register par
-rot OUT, 2  // Right rotate to 0b0100_0000
+mov r4, par //r4 now has p4
+set 2 
+rot r4, OUT
+mov r4, OUT
+set 2
+rot r4, OUT  // Right rotate to 0b0100_0000
 orr r3, OUT // Combines r3 with OUT(holds p4) and output to OUT
 mov r3, OUT // Update r3 to hold p4
 
@@ -45,7 +45,8 @@ and r2, r4         // lsb & 0b01101101
 xor OUT, r1        // OUT = parity(msb & 0b00000110) ^ (lsb & 0b01101101)
 
 // Fix with parity register par
-rot OUT, 3  // Right rot OUT to 0b0010_0000
+mov r4, 3
+rot OUT, r4  // Right rot OUT to 0b0010_0000
 orr r3, OUT // Combines p2 into r3 and sends output to OUT
 mov r3, OUT // Update r3 to hold p2
 ldr r1, r2  // Return r1 back to original value
@@ -61,7 +62,8 @@ and r2, R4         // OUT = (lsb & 0b01011011)
 xor OUT, r1        // OUT = parity((msb & 0b00000101) ^ (lsb & 0b01011011))
 
 // Fix with parity register par
-rot OUT, 4  // Turns OUT to 0b0001_0000
+mov r4, 4
+rot OUT, r4  // Turns OUT to 0b0001_0000
 orr r3, OUT // Combines parity with OUTput register from XOR
 mov r3, OUT
 ldr r1, r0  // Returns original value
@@ -74,21 +76,25 @@ mov r4, OUT
 xor r4, r3 // OUT now holds msb ^ lsb ^ p8 ^ p4 ^ p2 ^ p1
 
 // Fix with parity register par
-rot OUT, 5
+mov r4, 5
+rot OUT, r4
 orr r3, OUT
 mov r3, OUT
 
 
 // TRANSLATE msb rrt(3) & 11100000 | lsb rrt(3) & 00011111 | p8	output MSW
-rot r1, 3
+mov r4, 3
+rot r1, r4
 mov r4, 0b11100000
 and OUT, r4
 mov r1, OUT        // Replaces r1 with msb rrt(3) & 11100000
-rot r2, 3
+mov r4, 3
+rot r2, r4
 mov r4, 0b00011111
 and OUT, r4        // OUT now holds lsb rrt(3) & 00011111
 mov r2, OUT
-rot r3, 7          // Stores r3 rotated by 7 in OUT
+mov r4, 7
+rot r3, r4          // Stores r3 rotated by 7 in OUT
 mov r4, 0b00000001
 and r4, OUT
 mov r4, OUT        // R4 now holds p8 BIT
@@ -106,14 +112,17 @@ ldr r2, OUT        // r2 = mem[OUT] = mem[i+1] R2 now has LSB
 p2 rrt(6) & 11111100 | p1 rrt(7) & 11111110 | p0
 */
 
-// Blue Bold
-rot r2, 4
+//TODO fix rotates Blue Bold
+mov r4, 4
+rot r2, r4
 mov r4, 0b11110000
 and OUT, r4         // OUT = lsb rrt(4) & 11110000
 mov r4, OUT         // r4 holds lsb rrt(4) & 11110000
 mov OUT, 0b00000001
 and r2, OUT
-rot OUT, 5
+//TODO Check validity of statement
+mov r5, 5
+rot OUT, r5
 mov r2, OUT         // Replaces lsb with (lsb & 00000001) rrt(5)
 mov OUT, 0b11111000
 and r2, OUT         // r2 holds ((lsb & 00000001) rrt(5) & 11111000)
@@ -125,7 +134,8 @@ mov r2, OUT         // Now r2 holds lsb rrt(4) & 11110000 |((lsb & 00000001) rrt
 //Red Bold
 mov r4, 0b01000000
 and r3, r4		//Mask parity bits to grab p4 bit
-rot OUT, 10	//Rotates by 10 to get into lowest bit first
+mov r5, 10
+rot OUT, r5	//Rotates by 10 to get into lowest bit first
 mov r4, OUT 	//r4 now holds p4 rrt(4)
 mov OUT, 0b11110000
 and OUT, r4
@@ -137,7 +147,9 @@ mov r2, OUT	//r2 now holds the first 3 or statements (r4 free again)
 //Handling p2 bit
 mov r4, 0b00100000
 and r3, r4		//Mask parity bits to grab p2 bit
-rot OUT, 3
+//TODO Check validity
+mov r4, 3
+rot OUT, r4
 mov r4, OUT 	//r4 now holds p2 rrt(6)since it rotates twice
 mov OUT, 0b11111100
 and OUT, r4
@@ -149,7 +161,9 @@ mov r2, OUT	//r2 now holds the first 4 or statements (r4 free again)
 //Handling p1 bit
 mov r4, 0b00010000
 and r3, r4		//Mask parity bits to grab p1 bit
-rot OUT, 7
+//TODO Check validity
+mov r4, 7
+rot OUT, r4
 mov r4, OUT 	//r4 now holds p2 rrt(6)since it rotates twice
 mov OUT, 0b11111110
 and OUT, r4
@@ -161,7 +175,9 @@ mov r2, OUT	//r1 now holds the first 4 or statements (r4 free again)
 //Handling p0 case
 mov r4, 0b00001000
 and r4, r3
-rot OUT, 3
+//TODO Check validity
+mov r4, 3
+rot OUT, r4
 mov r4, OUT		//p4 now holds p0
 orr r2, r4
 mov r2, OUT		//r2 now holds out_lsw
