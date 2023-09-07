c0ce25d2828b91c2ab1349e39df61bd3e4012f1c
Nathan Thomas <nathanthomas707@gmail.com>
2023-09-06 21:49:07 -0700

    Update

--- program3.c
+++ program3.c
@@ -1,33 +1,63 @@
-Char pattern = mem[32];
-Int count = 0, byte_count = 0;
-For (int i = 0; i < 32; i++) {
-	Char b = mem[i];
-	
-	// parts a and b
-	//In ARM use “occurred” flag
-	Int occurred = 0;
-	If ((b & (1111_1000)) == pattern) {count++; occurred = 1}	// IF 1
-	//If ((b & (0111_1100)) == (pattern >> 1)) {count++; occurred = 1}
-    //If ((b & (0011_1110)) == (pattern >> 2)) {count++; occurred = 1}
-    //If ((b & (0001_1111)) == (pattern >> 3)) {count++; occurred = 1}
-    If (occurred){byte_count++;}	// IF 2
+int
+main()
+{
+    unsigned char mem[64];
+    char pattern = mem[32];
+    int count = 0, byte_count = 0, totalCount = 0;
+    for (int i = 0; i < 32; i++) {
+        char b = mem[i];
 
-    // part c
+        int occurred = 0;
+        if ((b & (0b11111000)) == pattern) {
+            count++;
+            occurred = 1;
+        }  // IF 1
+        if ((b & (0b01111100)) == (pattern >> 1)) {
+            count++;
+            occurred = 1;
+        }
+        if ((b & (0b00111110)) == (pattern >> 2)) {
+            count++;
+            occurred = 1;
+        }
+        if ((b & (0b00011111)) == (pattern >> 3)) {
+            count++;
+            occurred = 1;
+        }
+        if (occurred) {
+            byte_count++;
+        }  // IF 2
 
-    Int totalCount = 0;
-    //pattern >>= 3;
-    If (i != 31) {	// IF 3
+        // part c
 
-    //If(((b & 0000_1111) << 1) | (mem[i + 1] >> 7) == pattern) {totalCount++;}
-    If(((b & 0000_1111) << 1) | (mem[i + 1] rrt(7) & 0b00000001) == pattern){totalCount++;} IF4
-    //If(((b & 0000_0111) << 2) | (mem[i + 1] >> 6) == pattern) {totalCount++;}
-    If(((b & 0000_0111) << 2) | (mem[i + 1] rrt(6) & 0b00000011) == pattern){totalCount++;} IF5
-    //If(((b & 0000_0011) << 3) | (mem[i + 1] >> 5) == pattern) {totalCount++;}
-    If(((b & 0000_0011) << 3) | (mem[i + 1] rrt(5) & 0b00000111) == pattern){totalCount++;} IF6
-    //If(((b & 0000_0001) << 4) | (mem[i + 1] >> 4) == pattern) {totalCount++;}
-    If(((b & 0000_0001) << 4) | (mem[i + 1] rrt(4) & 0b00001111) == pattern){totalCount++;} IF7
+        pattern >>= 3;
+        if (i != 31) {  // IF 3
 
+            if ((((b & 0b00001111) << 1) | (mem[i + 1] >> 7)) == pattern) {
+                totalCount++;
+            }
+            // if (((b & 0000_1111) << 1) | (mem[i + 1] rrt(7) & 0b00000001) == pattern) {
+            //     totalCount++;
+            // }
+            if ((((b & 0b00000111) << 2) | (mem[i + 1] >> 6)) == pattern) {
+                totalCount++;
+            }
+            // if (((b & 0000_0111) << 2) | (mem[i + 1] rrt(6) & 0b00000011) == pattern) {
+            // totalCount++;
+            // }
+            if ((((b & 0b00000011) << 3) | (mem[i + 1] >> 5)) == pattern) {
+                totalCount++;
+            }
+            // if (((b & 0000_0011) << 3) | (mem[i + 1] rrt(5) & 0b00000111) == pattern) {
+            // totalCount++;
+            // }
+            if ((((b & 0b00000001) << 4) | (mem[i + 1] >> 4)) == pattern) {
+                totalCount++;
+            }
+            // if (((b & 0000_0001) << 4) | (mem[i + 1] rrt(4) & 0b00001111) == pattern) {
+            // totalCount++;
+            // }
+        }
+        totalCount += count;
     }
-    totalCount += count;
 }
-Mem
