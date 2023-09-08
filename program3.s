c71ad706273f39ca50fdfd94815e3f10099b159b
Nathan Thomas <nathanthomas707@gmail.com>
2023-09-07 20:22:01 -0700

    Fix bug in p3

--- program3.s
+++ program3.s
@@ -1,195 +1,363 @@
+# load pattern 
 
-# r0 - holds index i
-# r1 - general use
-# r2 - holds variable "count"
-# r3 - holds variable byte_count
-# r4 - holds variable totalCount
-# OUT - default output register
+mov r0, 0 ; r0 = i = 0
 
-# Note: need to fix some incorrect operands and one of the comments creates a Unicode error
+# count = mem[33]
+# byte_count = mem[34]
+# totalCount = mem[35]
+# initialize mem to 0
 
-// Initialize variables
-mov r0, 0                // int i = 0
-mov r1, 32
-mov r2, 0                // count = 0
-mov r3, 0                // byte_count = 0        
+set 33
+str r0, OUT
+set 34
+str r0, OUT
+set 35
+str r0, OUT
 
-// For loop 
-set loop_end
-beq r0, r1               //  Stop when r0(i) = r1(32)              
+# We will be directly using memory to keep
+# track of the counts
 
-// parts a and b
-// If ((b & (1111_1000)) == pattern) {count++; occurred = 1}
-mov r3, 0b11111000      // byte_count not needed for 1st If, can use r3
-and r4, r3              // OUT = b & (1111_1000)            
+loop_start:
+mov r0, r0 ; nop
 
-mov r5, 32
-ldr r1, r5              // r1 = pattern = mem[32]
+
+; make sure r1 contains pattern
+set 32
+ldr r1, OUT ; r1 = pattern = mem[32]
+
+# load r2 with b
+ldr r2, r0 ; r2 = mem[i]
+
+mov r3, 0 ; r3 = count = 0
+
+mov r4, 0b11111000
+and r2, r4
+mov r2, OUT ; r2 = b & 0b11111000
 set if1
-beq OUT, r1             // b & (1111_1000) == pattern                        
+bne r2, r1
+
+set 1
+add r3, OUT
+mov r3, OUT ; r3 = count += 1
 
 if1:
-mov r0, r0
-set 1                   // OUT = 1
-add r2, OUT             // OUT = r2 + 1
-mov r2, OUT             // count++
-mov r1, 1               // r1 = occurred = 1 (Remember to return r1 to 32 for the for loop)
-             
+mov r0, r0 ; nop
+ldr r2, r0
+
+# rotate mask
+set 1
+rot r4, OUT
+mov r4, OUT ; r4 >>= 1
+
+# mask byte and shift left
+and r2, r4
+mov r2, OUT ; r2 = b & 0b01111100
+set 7
+rot r2, OUT
+mov r2, OUT
+
 set if2
-mov r5, 1
-beq r1, r5              // If (occurred) byte_count++           
+bne r2, r1
+
+set 1
+add r3, OUT
+mov r3, OUT ; r3 = count += 1
 
 if2:
-mov r0, r0
-mov r5, 1
-add r3, r5              // OUT = r3 + 1 
-mov r3, OUT             //byte_count++
+mov r0, r0 ; nop
+ldr r2, r0
 
-// part c
-mov r1, 0               // totalCount = 0
+set 1
+rot r4, OUT
+mov r4, OUT ; r4 >>= 1
+
+# mask byte and shift left
+and r2, r4
+mov r2, OUT ; r2 = b & 0b00111110
+set 6
+rot r2, OUT
+mov r2, OUT
+
 set if3
-mov r5, 31
-beq r0, r5              // if(i != 31), branch when i = 31
+bne r2, r1
 
-// If(((b & 0000_1111) << 1) | (mem[i + 1] rrt(7) & 0b00000001) == pattern)
 set 1
-add r0, OUT             // OUT = i + 1
-ldr r1, r0              // r1 = mem[OUT] = mem[i+1]
-set 7                   // OUT = 7
-rot r1, OUT             // mem[i + 1] rrt(7)
-set 0b00000001
-and r1, OUT             // OUT = C = (mem[i + 1] rrt(7) & 0b00000001) 
-mov r1, OUT             // (mem[i + 1] rrt(7) & 0b00000001)      
-
-// (b & 0000_1111) << 1
-ldr r5, r0              // b = mem[i]
-set 0b00001111
-and r5, OUT             // OUT = b & 0b00001111
-mov r5, 7
-rot OUT, r5             // OUT = (b & 0000_1111) << 1         
-
-orr OUT, r1             // OUT = ((b & 0000_1111) << 1) | (mem[i + 1] rrt(7) & 0b00000001)
-
-mov r5, 32
-ldr r1, r5
-set if4
-beq OUT, r1             // ((b & 0000_1111) << 1) | (mem[i + 1] rrt(7) & 0b00000001) == pattern)
-     
-if4:
-mov r0, r0
-set 1                   // OUT = 1
-add r4, OUT             // OUT = r4 + 1
-mov r4, OUT             // r4 = r4 + 1 = totalCount++             
-
-// Fourth if statement
-// (mem[i + 1] rrt(7) & 0b00000001)
-set 1
-add r0, OUT             // OUT = i + 1
-ldr r1, OUT             // r1 = mem[OUT] = mem[i+1]
-set 6                   // OUT = 6
-rot r1, OUT                                  
-set 0b00000011
-and r1, OUT             // OUT = C = (mem[i + 1] rrt(6) & 0b00000011)
-mov r1, OUT             // r1 holds OUT for later OR              
-
-// ((b & 0000_0111) << 2)
-ldr r5, r0              // b = mem[i]
-set 0b00000111
-and r5, OUT             // OUT = b & 0b00000111
-mov r5, 6
-rot OUT, r5             // OUT = ((b & 0000_0111) << 2)        
-
-// ((b & 0000_0111) << 2) | (mem[i + 1] rrt(6) & 0b00000011)
-orr OUT, r1
-
-mov r5, 32
-ldr r1, r5
-set if5
-beq OUT, r1             // (((b & 0000_1111) << 1) | (mem[i + 1] rrt(7) & 0b00000001) == pattern)             
-
-// Increment totalCount
-if5:
-mov r0, r0
-set 1                   // OUT = 1
-add r4, OUT             // OUT = r4 + 1
-mov r4, OUT             // r4 = r4 + 1 = totalCount++      
-
-// Fifth if statement
-// (mem[i + 1] rrt(5) & 0b00000111)
-set 1
-add r0, OUT             // OUT = i + 1
-ldr r1, OUT             // r1 = mem[OUT] = mem[i+1]
-set 5                   // OUT = 5
-rot r1, OUT                                        
-set 0b00000111
-and r1, OUT             // OUT = C = (mem[i + 1] rrt(5) & 0b00000111) 
-mov r1, OUT             // r1 = mem[i + 1] rrt(5) & 0b00000111 
-
-// ((b & 0000_0011) << 3)
-ldr r5, r0
-set  0b00000011
-and r5, OUT             // OUT = (b & 0000_0011)
-mov r5, 5
-rot OUT, r5             // OUT = (b & 0b00000011) << 3  
-
-//((b & 0000_0011) << 3) | (mem[i + 1] rrt(5) & 0b00000111)
-orr OUT, r1
-
-mov r5, 32
-ldr r1, r5              // r1 = pattern = mem[32]
-set if6
-beq OUT, r1             // (((b & 0000_1111) << 1) | (mem[i + 1] rrt(7) & 0b00000001) == pattern)     
-
-// Increment totalCount
-if6:
-mov r0, r0
-set 1                   // OUT = 1
-add r4, OUT             // OUT = r4 + 1
-mov r4, OUT             // r4 = r4 + 1 = totalCount++          
-
-// Sixth if statement
-// (mem[i + 1] rrt(4) & 0b00001111)
-set 1
-add r0, OUT             // OUT = i + 1
-ldr r1, r0              // r1 = mem[OUT] = mem[i+1]
-set 4                   // OUT = 4
-rot r1, OUT                                        
-set 0b00001111
-and r1, OUT             // OUT = C = (mem[i + 1] rrt(4) & 0b00001111)
-mov r1, OUT             // r1 = mem[i + 1] rrt(4) & 0b00001111
-
-// (b & 0000_0001) << 4
-ldr r5, r0
-set 0b00000001
-and r5, OUT             // OUT = b & 0000_0001
-mov r5, 4
-rot OUT, r5             // (b & 0000_0001) << 4         
-
-// ((b & 0000_0001) << 4) | (mem[i + 1] rrt(4) & 0b00001111)
-orr OUT, r1
-
-mov r5, 32
-ldr r1, r5              // r1 = pattern = mem[32]
-set if7
-beq OUT, r1             // (((b & 0000_1111) << 1) | (mem[i + 1] rrt(7) & 0b00000001) == pattern)
-
-// Increment totalCount
-if7:
-mov r0, r0
-set 1                   // OUT = 1
-add r4, OUT             // OUT = r4 + 1
-mov r4, OUT             // r4 = r4 + 1 = totalCount++         
+add r3, OUT
+mov r3, OUT ; r3 = count += 1
 
 if3:
-mov r0, r0
-add r4 r2               // OUT = totalCount + count           
-mov r4, OUT             // totalCount = totalCount + count
+mov r0, r0 ; nop
+ldr r2, r0
 
-// increment r0 by 1 for next loop
-mov r1, 1
-add r0, r1              // r1 can be used after assigning b (OUT = increment i by 1)
-mov r0, OUT             // i is incremented for next loop (r0 = i + 1)
-mov r1, 1               // Return r1 to 32 for for loop
+set 1
+rot r4, OUT
+mov r4, OUT ; r4 >>= 1
 
-loop_end:
+# mask byte and shift left
+and r2, r4
+mov r2, OUT ; r2 = b & 0b00011111
+set 5
+rot r2, OUT
+mov r2, OUT
+
+set if4
+bne r2, r1
+
+set 1
+add r3, OUT
+mov r3, OUT ; r3 = count += 1
+
+if4:
+mov r0, r0 ; nop
+
+# if r3 != 0, increment byte count
+mov r2, 0
+set byte_count_increment
+bne r3, r2
+
+# add 1 to r2 so that it is no longer equal to r3
+set 1
+add r2, OUT
+mov r2, OUT
+set skip_byte_count_increment
+bne r3, r2 ; unconditional branch
+
+byte_count_increment:
+mov r0, r0 ; nop
+
+# load, increment, and store byte count
+set 34
+ldr r2, OUT ; r2 = mem[34]
+set 1
+add r2, OUT
+mov r2, OUT ; r2 += 1
+set 34
+str r2, OUT ; mem[34] = r2
+
+skip_byte_count_increment:
 mov r0, r0
+
+# mem[33] += count
+set 33
+ldr r4, OUT ; r4 = mem[33]
+add r4, r3
+mov r4, OUT ; r4 += count
+set 33
+str r4, OUT ; mem[33] = r4
+
+# mem[35] += count
+
+set 35
+ldr r4, OUT ; r4 = mem[33]
+add r4, r3
+mov r4, OUT ; r4 += count
+set 35
+str r4, OUT ; mem[33] = r4
+
+# now r3 is free
+
+# we only want to do this if we are not in
+# the last iteration, so skip to loop end
+# if i == 31
+mov r3, 31
+set calc_totalCount
+bne r0, r3
+
+mov r3, 33
+set if24
+bne r0, r3 ; unconditional branch, i never equal to 33
+
+
+# r0: i
+# r1: pattern
+# r2: b
+# r3: mem[i+1]
+# r4 free
+calc_totalCount:
+mov r0, r0 ; nop
+ldr r2, r0 ; r2 = mem[i] = b
+
+// pattern >>= 3
+set 3
+rot r1, OUT
+mov r1, OUT
+
+set 1
+add r0, OUT
+ldr r3, OUT ; r3 = mem[i+1]
+
+set 0b00001111
+and r2, OUT
+mov r2, OUT
+set 7
+rot r2, OUT
+mov r2, OUT ; r2 = b & 0b00001111 << 1
+
+// r2 NO LONGER b
+
+set 7
+rot r3, OUT
+mov r4, OUT ; r4 = mem[i+1] rot(7)
+set 0b00000001
+and r4, OUT ; only keep last bit
+orr OUT, r2
+mov r4, OUT ; r4 = ((b & 0b00001111) << 1) | (mem[i + 1] >> 7)
+
+// restore r2
+ldr r2, r0
+
+set if21
+bne r4, r1
+
+# increment mem[35]
+set 35
+ldr r4, OUT
+set 1
+add r4, OUT
+mov r4, OUT
+set 35
+str r4, OUT
+
+if21:
+mov r0, r0
+
+# r0: i
+# r1: b
+# r2: mem[i+1]
+# r3, r4 free
+
+set 1
+add r0, OUT
+ldr r3, OUT ; r3 = mem[i+1]
+
+set 0b0000111
+and r2, OUT
+mov r2, OUT
+set 6
+rot r2, OUT
+mov r2, OUT ; r2 = b & 0b00001111 << 1
+
+// r2 NO LONGER b
+
+set 6
+rot r3, OUT
+mov r4, OUT ; r4 = mem[i+1] rot(7)
+set 0b00000011
+and r4, OUT ; only keep last bit
+orr OUT, r2
+mov r4, OUT ; r4 = ((b & 0b00001111) << 1) | (mem[i + 1] >> 7)
+
+// restore r2
+ldr r2, r0
+
+set if22
+bne r4, r1
+
+# increment mem[35]
+set 35
+ldr r4, OUT
+set 1
+add r4, OUT
+mov r4, OUT
+set 35
+str r4, OUT
+
+if22:
+mov r0, r0
+
+# r0: i
+# r1: b
+# r2: mem[i+1]
+# r3, r4 free
+
+set 1
+add r0, OUT
+ldr r3, OUT ; r3 = mem[i+1]
+
+set 0b0000011
+and r2, OUT
+mov r2, OUT
+set 5
+rot r2, OUT
+mov r2, OUT ; r2 = b & 0b00001111 << 1
+
+// r2 NO LONGER b
+
+set 5
+rot r3, OUT
+mov r4, OUT ; r4 = mem[i+1] rot(7)
+set 0b00000111
+and r4, OUT ; only keep last bit
+orr OUT, r2
+mov r4, OUT ; r4 = ((b & 0b00001111) << 1) | (mem[i + 1] >> 7)
+
+// restore r2
+ldr r2, r0
+
+set if23
+bne r4, r1
+
+# increment mem[35]
+set 35
+ldr r4, OUT
+set 1
+add r4, OUT
+mov r4, OUT
+set 35
+str r4, OUT
+
+if23:
+mov r0, r0
+
+
+# r0: i
+# r1: b
+# r2: mem[i+1]
+# r3, r4 free
+
+set 1
+add r0, OUT
+ldr r3, OUT ; r3 = mem[i+1]
+
+set 0b0000001
+and r2, OUT
+mov r2, OUT
+set 4
+rot r2, OUT
+mov r2, OUT ; r2 = b & 0b00001111 << 1
+
+// r2 NO LONGER b
+
+set 4
+rot r3, OUT
+mov r4, OUT ; r4 = mem[i+1] rot(7)
+set 0b00001111
+and r4, OUT ; only keep last bit
+orr OUT, r2
+mov r4, OUT ; r4 = ((b & 0b00001111) << 1) | (mem[i + 1] >> 7)
+
+// restore r2
+ldr r2, r0
+
+set if24
+bne r4, r1
+
+# increment mem[35]
+set 35
+ldr r4, OUT
+set 1
+add r4, OUT
+mov r4, OUT
+set 35
+str r4, OUT
+
+if24:
+mov r0, r0
+
+set 1
+add r0, OUT
+mov r0, OUT ; i = r0 += 1
+
+mov r2, 32
+set loop_start
+bne r0, r2
